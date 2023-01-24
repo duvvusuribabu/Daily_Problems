b08c67248c196ddc5e01be7aede20685cc5c8bd7
Suribabu_Duvvu <89627322+duvvusuribabu@users.noreply.github.com>
2023-01-24 21:22:23 +0530

    Create Inorder.java

---
+++ 24Jan2023/Inorder.java
@@ -0,0 +1,14 @@
+//Inorder in java
+
+class Solution {
+    ArrayList<Integer> list = new ArrayList<>();
+    public List<Integer> inorderTraversal(TreeNode root) {
+        if(root==null)
+          return list;
+        inorderTraversal(root.left);
+        list.add(root.val);
+        inorderTraversal(root.right);
+
+        return list;  
+    }
+}
