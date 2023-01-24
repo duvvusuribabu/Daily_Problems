e4c5ce50cc623ae7716e06eb2645b684bdb450a5
Suribabu_Duvvu <89627322+duvvusuribabu@users.noreply.github.com>
2023-01-24 21:24:34 +0530

    Create SameTree.java

---
+++ 24Jan2023/SameTree.java
@@ -0,0 +1,7 @@
+// Tree Comparisison
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        return (p==null && q==null)|| (p != null&& q != null && p.val==q.val && isSameTree(p.left,q.left)&& isSameTree(p.right,q.right));
+        
+    }
+}
