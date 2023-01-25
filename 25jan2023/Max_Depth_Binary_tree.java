4f6bb3bebe730378c0c5ddf651bd39375e1dce82
Suribabu_Duvvu <89627322+duvvusuribabu@users.noreply.github.com>
2023-01-25 19:33:53 +0530

    Create Max_Depth_Binary_tree.java

---
+++ 25jan2023/Max_Depth_Binary_tree.java
@@ -0,0 +1,13 @@
+// Maximum Depth of a binary tree
+
+class Solution {
+    
+    public int maxDepth(TreeNode root) {
+        if(root==null)
+         return 0;
+        int lh = maxDepth(root.left);
+        int rh = maxDepth(root.right);
+        return 1+Math.max(lh,rh);
+
+    }
+}
